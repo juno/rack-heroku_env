756c113ea185ffccee8d1b3563568c9215aedbdf
Brian Landau <brianjlandau@gmail.com>
2012-05-01 10:59:13 -0600

    Be more dynamic about how we generate headers

--- lib/rack/heroku_env.rb
+++ lib/rack/heroku_env.rb
@@ -2,24 +2,29 @@ module Rack
   class HerokuEnv
     VERSION = "0.0.1"
 
-    VARIABLE_NAMES = {
-      'HTTP_X_HEROKU_DYNOS_IN_USE' => 'X-Heroku-Dynos-In-Use',
-      'HTTP_X_HEROKU_QUEUE_DEPTH' => 'X-Heroku-Queue-Depth',
-      'HTTP_X_HEROKU_QUEUE_WAIT_TIME' => 'X-Heroku-Queue-Wait-Time',
-    }
-
     def initialize(app)
       @app = app
     end
 
     def call(env)
       response = @app.call(env)
-
-      VARIABLE_NAMES.each do |variable_name, header_name|
-        response[1][header_name] = env[variable_name] if env[variable_name]
+      
+      heroku_vars(env).each do |variable_name|
+        response[1][header_name_for(variable_name)] = env[variable_name]
       end
 
       response
+    end
+    
+    private
+    
+    
+    def heroku_vars(env)
+      env.keys.grep(/\AHTTP_X_HEROKU/)
+    end
+    
+    def header_name_for(variable_name)
+      variable_name.gsub(/\AHTTP_/, '').split('_').map{|w| w.capitalize }.join('-')
     end
 
   end
