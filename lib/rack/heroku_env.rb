8543ad28724b276deff86a328afcbb4b128f753d
Junya Ogura <junyaogura@gmail.com>
2012-05-03 10:37:07 +0900

    cleanup code.

--- lib/rack/heroku_env.rb
+++ lib/rack/heroku_env.rb
@@ -8,21 +8,21 @@ module Rack
 
     def call(env)
       response = @app.call(env)
-      
+
       heroku_vars(env).each do |variable_name|
         response[1][header_name_for(variable_name)] = env[variable_name]
       end
 
       response
     end
-    
+
     private
-    
-    
+
+
     def heroku_vars(env)
       env.keys.grep(/\AHTTP_X_HEROKU/)
     end
-    
+
     def header_name_for(variable_name)
       variable_name.gsub(/\AHTTP_/, '').split('_').map{|w| w.capitalize }.join('-')
     end
